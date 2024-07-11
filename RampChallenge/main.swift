05c84bb8ddffd023676fe5fab2b87393afe28d23
Justin Vallely <justin.vallely@ibotta.com>
2024-07-11 15:46:15 -0600

    adds parsing for flag

--- RampChallenge/main.swift
+++ RampChallenge/main.swift
@@ -22,7 +22,7 @@ func getChallengeData(completion: @escaping (Result<String, Error>) -> Void) {
     task.resume()
 }
 
-func parseHiddenUrl(from html: String) -> String {
+func parseHiddenUrl(from html: String) -> URL? {
     let characterSearch = /(<code class="ramp").*?<div class="ramp".*?<span class="ramp".*?<i class="ramp char" value="(?<character>.{1})/
 
     var combinedString: String = ""
@@ -32,15 +32,33 @@ func parseHiddenUrl(from html: String) -> String {
         combinedString += result.character
     }
 
-    return combinedString
+    return URL(string: combinedString)
+}
+
+func captureTheFlag(url: URL) {
+    let session = URLSession.shared
+    let request = URLRequest(url: url)
+
+    let task = session.dataTask(with: request) { data, URLResponse, error in
+        if let error = error {
+            print("Error: \(error)")
+            exit(1)
+        } else if let data = data {
+            print("Data received: \(data)")
+            guard let dataString = String(data: data, encoding: .utf8) else { print("Error encoding flag data"); exit(1) }
+            print("Flag 👉 " + "\(dataString)")
+            exit(0)
+        }
+    }
+    task.resume()
 }
 
 getChallengeData() { result in
     switch result {
     case .success(let html):
-        let url = parseHiddenUrl(from: html)
-        print("Hidden URL:" + "\(url)")
-        exit(0)
+        guard let url = parseHiddenUrl(from: html) else { print("Error parsing Hidden URL"); exit(1) }
+        print("Hidden URL 👉 " + "\(url)")
+        captureTheFlag(url: url)
     case .failure(let error):
         print("Error: \(error)")
         exit(1)
